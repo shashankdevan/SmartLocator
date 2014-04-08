baf53781cde3e7afeb025692745dee7663a32aca
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-07 23:20:25 -0400

    Comments

--- src/com/example/smartlocator/MainActivity.java
+++ src/com/example/smartlocator/MainActivity.java
@@ -24,6 +24,10 @@ import android.os.Handler;
 import android.app.Activity;
 import android.app.Service;
 
+/*
+ * A single monolithic class which performs everything :(
+ * This is breaking the Single Responsibility Principle, need to fix as soon as possible!
+ */
 public class MainActivity extends Activity implements LocationListener {
 
     public static final String WARN = "WARNING";
@@ -33,7 +37,8 @@ public class MainActivity extends Activity implements LocationListener {
     public static final int GPS_UPDATE_INTERVAL = 60000;
     public static final int DEFAULT_ZOOM_LEVEL = 18;
 
-    final private double STEP_SIZE = 0.419;
+    final private double STEP_SIZE = 0.838;
+    public static final int MIN_STEP_TIME = 200;
     final private double METER_PER_LAT_DEGREE = 78095.9773719797;
     final private double METER_PER_LNG_DEGREE = 90163.65604055098;
     final private int MAGNETIC_DECLINATION = 0;
@@ -42,28 +47,24 @@ public class MainActivity extends Activity implements LocationListener {
     private LocationManager locationManager;
     private SensorManager sensorManager;
     private SensorListener sensorListener = new SensorListener();
-
     private Handler locationUpdateHandler = new Handler();
+
     private boolean isFirstUpdate = true;
     private long currentPeakTime;
     private long lastKnownPeakTime;
     private float acclMagnitude = 0;
-
     private boolean acclDirection = true;
-
     private int stepCount = 0;
-    public static final int MIN_STEP_TIME = 200;
-    private float[] acclReadings = new float[3];
 
+    private float[] acclReadings = new float[3];
     private float[] filteredAccl = new float[3];
     private boolean readAccl = false;
     private float[] magnReadings = new float[3];
-
     private float[] filteredMagn = new float[3];
     private boolean readMagn = false;
+
     private long lastMarkerUpdateTime;
     private double lastLat, lastLng;
-
     private CameraPosition cameraPosition = null;
 
     @Override
@@ -82,23 +83,36 @@ public class MainActivity extends Activity implements LocationListener {
     protected void onResume() {
         super.onResume();
 
+        /*
+         * Register for the Accelerometer and Magnetometer so that we can use them to update the location
+         */
         Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         Sensor magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         sensorManager.registerListener(sensorListener, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+        /*
+         * Register for location updates from GPS if GPS is enabled, else prompt a dialog asking to enable GPS
+         * If a user declines to enable GPS, we try to get the best known stored location from either GPS or Network
+         */
         if (locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, GPS_UPDATE_INTERVAL, 0, this);
         else
             updateBestKnownLocation();
 
+        /*
+         * Bring back the camera where it was before pausing
+         */
         if (cameraPosition != null) {
             map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             cameraPosition = null;
         }
     }
 
+    /*
+     * Unregister updates from the sensors as well as GPS
+     */
     @Override
     protected void onPause() {
         super.onPause();
@@ -107,6 +121,10 @@ public class MainActivity extends Activity implements LocationListener {
         cameraPosition = map.getCameraPosition();
     }
 
+    /*
+     * Prompt a user with a dialog to enable GPS. If they decline then take the best known stored location
+     * This is called only on the first time after the Activity resumes
+     */
     private void updateBestKnownLocation() {
         showEnableGpsDialog();
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -126,6 +144,9 @@ public class MainActivity extends Activity implements LocationListener {
         }
     }
 
+    /*
+     * Dialog to prompt the user to enable the GPS. If they choose the settings option, the Settings activity is opened
+     */
     private void showEnableGpsDialog() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
         alertDialog.setTitle("GPS Settings");
@@ -147,13 +168,18 @@ public class MainActivity extends Activity implements LocationListener {
         alertDialog.show();
     }
 
+    /*
+     * Update the location fields and animate the camera to that location.
+     */
     private void updateLocationOnGpsDisabled(Location location) {
         lastLat = location.getLatitude();
         lastLng = location.getLongitude();
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLat, lastLng), DEFAULT_ZOOM_LEVEL));
     }
 
-
+    /*
+     * Listener class which is subscribed to updates from Accelerometer and Magnetometer
+     */
     public class SensorListener implements SensorEventListener {
 
         @Override
@@ -163,6 +189,9 @@ public class MainActivity extends Activity implements LocationListener {
 
         @Override
         public void onSensorChanged(SensorEvent event) {
+            /*
+             * Store the Accelerometer and Magnetometer readings after applying a low pass filter
+             */
             switch (event.sensor.getType()) {
                 case Sensor.TYPE_ACCELEROMETER:
                     lowPassAccl(event);
@@ -179,10 +208,16 @@ public class MainActivity extends Activity implements LocationListener {
                     Log.d(WARN, "Invalid Sensor Received");
             }
 
+            /*
+             * Asynchronously update the location using the sensors
+             */
             SensorLocationRefresher task = new SensorLocationRefresher();
             locationUpdateHandler.post(task);
         }
 
+        /*
+         * When a pedestrian is walking or running, detect their steps using the peaks in the accelerometer values
+         */
         private void detectStep(float currentAcclMagnitude) {
             if (acclDirection) {
                 if (currentAcclMagnitude > acclMagnitude) {
@@ -206,12 +241,18 @@ public class MainActivity extends Activity implements LocationListener {
             Log.d("STEP", String.valueOf(stepCount));
         }
 
+        /*
+         * Calculate the magnitude of the accelerometer values after applying a high pass filter
+         */
         private float getMagnitude(SensorEvent event) {
             return (float) Math.sqrt(Math.pow(event.values[0] - filteredAccl[0], 2)
                     + Math.pow(event.values[1] - filteredAccl[1], 2)
                     + Math.pow(event.values[2] - filteredAccl[2], 2));
         }
 
+        /*
+         * Low pass filter for accelerometer values
+         */
         private void lowPassAccl(SensorEvent event) {
             final float alpha = (float) 0.8;
 
@@ -219,6 +260,9 @@ public class MainActivity extends Activity implements LocationListener {
                 filteredAccl[i] = alpha * filteredAccl[i] + (1 - alpha) * event.values[i];
         }
 
+        /*
+         * Low pass filter for magnetometer values
+         */
         private void lowPassMagn(SensorEvent event) {
             final float alpha = (float) 0.8;
 
@@ -255,6 +299,9 @@ public class MainActivity extends Activity implements LocationListener {
 
     private class SensorLocationRefresher implements Runnable {
 
+        /*
+         * Update the marker on calculating the orientation and distance after a small interval
+         */
         @Override
         public void run() {
             if (readAccl && readMagn && (System.currentTimeMillis() - lastMarkerUpdateTime) > MARKER_UPDATE_INTERVAL) {
@@ -264,6 +311,9 @@ public class MainActivity extends Activity implements LocationListener {
                 float[] I = new float[9];
                 float[] orientation = new float[3];
 
+                /*
+                 * Calculate the orientation in world system using the rotation matrix
+                 */
                 SensorManager.getRotationMatrix(R, I, acclReadings, magnReadings);
                 SensorManager.getOrientation(R, orientation);
 
@@ -272,6 +322,10 @@ public class MainActivity extends Activity implements LocationListener {
             }
         }
 
+        /*
+         * Given the angle in horizontal plane (azimuth), update the location in the same
+         * direction using the steps counted till now
+         */
         private void updateLocation(double azimuthalAngle) {
             lastLat += (stepCount * STEP_SIZE) * Math.cos(azimuthalAngle) / METER_PER_LAT_DEGREE;
             lastLng += (stepCount * STEP_SIZE) * Math.sin(azimuthalAngle) / METER_PER_LNG_DEGREE;
@@ -279,6 +333,9 @@ public class MainActivity extends Activity implements LocationListener {
             placeMarker(new LatLng(lastLat, lastLng));
         }
 
+        /*
+         * Place the marker at the position given in the parameter
+         */
         public void placeMarker(LatLng position) {
             map.clear();
             map.addMarker(new MarkerOptions()
@@ -290,6 +347,9 @@ public class MainActivity extends Activity implements LocationListener {
     }
 
 
+    /*
+     * Runnable which takes the updates the location using the GPS location values
+     */
     private class GpsLocationRefresher implements Runnable {
 
         Location location;
